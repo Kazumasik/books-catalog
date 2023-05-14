280cf41fe94c2196674d923d36ad2358bb4be4d6
Maxim <hhbbj63@gmail.com>
2023-05-14 23:35:01 +0300

    add registration dialog

--- src/components/header/LoginButton.vue
+++ src/components/header/LoginButton.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { reactive } from "vue";
 
+const dialogRegister = ref(false);
 const dialogLogin = ref(false);
 const valid = ref(false);
 
@@ -10,48 +11,117 @@ const registrationData = reactive({
   password: "",
   repeatPassword: "",
 });
+
+const loginData = reactive({
+  email: "",
+  password: "",
+});
+const isPasswordVisible = ref(false);
+
+const changeModal = () => {
+  dialogRegister.value = !dialogRegister.value;
+  dialogLogin.value = !dialogLogin.value;
+};
 </script>
 <template>
-  <v-btn variant="flat">
-    Увійти
-    <v-dialog v-model="dialogLogin" activator="parent" max-width="600px">
-      <v-card>
-        <v-card-title class="text-h5">Реєстрація</v-card-title>
-        <VCardText class="pt-0">
-          <VContainer class="pt-0">
-            <v-form @submit.prevent v-model="valid">
-              <v-text-field
-                variant="outlined"
-                v-model="registrationData.nickname"
-                label="Нікнейм"
-                required
-              ></v-text-field>
-              <v-text-field
-                variant="outlined"
-                v-model="registrationData.email"
-                label="Пошта"
-                required
+  <v-btn variant="flat" @click="dialogLogin=true"> Увійти </v-btn>
+  <v-dialog v-model="dialogLogin" max-width="600px">
+    <v-card>
+      <v-card-title class="text-h5">Вхід</v-card-title>
+      <VCardText class="pt-0">
+        <VContainer class="pt-0">
+          <v-form @submit.prevent v-model="valid">
+            <v-text-field
+              variant="outlined"
+              v-model="loginData.email"
+              label="Пошта"
+              required
+            >
+            </v-text-field>
+            <v-text-field
+              variant="outlined"
+              v-model="loginData.password"
+              label="Пароль"
+              :type="isPasswordVisible ? 'text' : 'password'"
+              :append-inner-icon="
+                isPasswordVisible ? 'mdi-eye-off-outline' : 'mdi-eye-outline'
+              "
+              @click:append-inner="isPasswordVisible = !isPasswordVisible"
+              required
+            ></v-text-field>
+            <v-btn type="submit" class="mt-2 w-100">Увійти</v-btn>
+            <VCol cols="12" class="text-center text-base mt-2 pa-0">
+              <span>Ще немає аккаунту?</span>
+              <v-btn
+                @click="changeModal"
+                variant="text"
+                class="text-subtitle-1 ms-2"
               >
-              </v-text-field>
-              <v-text-field
-                variant="outlined"
-                v-model="registrationData.password"
-                label="Пароль"
-                required
-              ></v-text-field>
-              <v-text-field
-                variant="outlined"
-                v-model="registrationData.repeatPassword"
-                label="Повторіть пароль"
-                required
-              ></v-text-field>
-              <v-btn type="submit" class="mt-2">Зареєструватись</v-btn>
-            </v-form>
-          </VContainer>
-        </VCardText>
-      </v-card>
-    </v-dialog>
-  </v-btn>
+                Зареєструйтесь
+              </v-btn>
+            </VCol>
+          </v-form>
+        </VContainer>
+      </VCardText>
+    </v-card>
+  </v-dialog>
+  <v-dialog v-model="dialogRegister" max-width="600px">
+    <v-card>
+      <v-card-title class="text-h5">Реєстрація</v-card-title>
+      <VCardText class="pt-0">
+        <VContainer class="pt-0">
+          <v-form @submit.prevent v-model="valid">
+            <v-text-field
+              variant="outlined"
+              v-model="registrationData.nickname"
+              label="Нікнейм"
+              required
+            ></v-text-field>
+            <v-text-field
+              variant="outlined"
+              v-model="registrationData.email"
+              label="Пошта"
+              required
+            >
+            </v-text-field>
+            <v-text-field
+              variant="outlined"
+              v-model="registrationData.password"
+              label="Пароль"
+              :type="isPasswordVisible ? 'text' : 'password'"
+              :append-inner-icon="
+                isPasswordVisible ? 'mdi-eye-off-outline' : 'mdi-eye-outline'
+              "
+              @click:append-inner="isPasswordVisible = !isPasswordVisible"
+              required
+            ></v-text-field>
+            <v-text-field
+              variant="outlined"
+              v-model="registrationData.repeatPassword"
+              label="Повторіть пароль"
+              :type="isPasswordVisible ? 'text' : 'password'"
+              :append-inner-icon="
+                isPasswordVisible ? 'mdi-eye-off-outline' : 'mdi-eye-outline'
+              "
+              @click:append-inner="isPasswordVisible = !isPasswordVisible"
+              required
+            ></v-text-field>
+            <v-btn type="submit" class="mt-2 w-100">Зареєструватись</v-btn>
+            <VCol cols="12" class="text-center text-base mt-2 pa-0">
+              <span>Уже є аккаунт?</span>
+              <v-btn
+                @click="changeModal"
+                variant="text"
+                class="text-subtitle-1 ms-2"
+              >
+                Увійдіть
+              </v-btn>
+            </VCol>
+          </v-form>
+        </VContainer>
+      </VCardText>
+    </v-card>
+  </v-dialog>
 </template>
 <style scoped>
 .v-card-title {
